1a7b1f29f214bb44aa2f05dbcf93ec51994bdb33
mshahid1996 <Mohd.Shahid@moreyeahs.in>
2022-08-29 15:16:07 +0530

    Add existing project files to Git

---
+++ ClassLibrary4/CreateAccountPlugin.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary4
+{
+    [CrmPluginRegistration("Create", 
+    "account", StageEnum.PreOperation, ExecutionModeEnum.Synchronous,
+    "","PreCreate Account", 1, 
+    IsolationModeEnum.Sandbox 
+    ,Id = "f3a94f1c-7627-ed11-9db1-00224823e0ed" 
+    )]
+    public class CreateAccountPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            //TODO:
+        }
+    }
+}
